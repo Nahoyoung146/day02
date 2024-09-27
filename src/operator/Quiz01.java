e1ad24abea0a2a7e88a77ba47f1874afb0a6e3b9
hoyoung <skghdud024@naver.com>
2024-09-27 11:36:27 +0900

    Quiz수정

--- src/operator/Quiz01.java
+++ src/operator/Quiz01.java
@@ -9,14 +9,14 @@ public class Quiz01 {
 		System.out.print("수 입력 : ");
 		int num1 = scan.nextInt();
 		String a = (num1 % 2 == 0 && num1 % 3 != 0) ? num1 + " = 짝수\n" + num1 + " = 3의 배수가 아니다."
-				: num1 + " = 홀수\n" + num1 + " = 3의 배수이다.";
+				: num1 + "짝수인데 3의 배수 혹은 " + num1 + "홀수인데 3의 배수일수도 있고 아닐수도 있다.";
 		System.out.println(a);
 
 		System.out.print("두 수 입력 : ");
 		int num2 = scan.nextInt();
 		int num3 = scan.nextInt();
-		String b = (num2 > num1) ? "num1 : " + num2 + " ,num2 : " + num3 + "\nnum2가 num1보다 크다"
-				: "num1 : " + num2 + " ,num2 : " + num3 + "\nnum1가 num2보다 크다";
+		String b = (num2 > num1) ? "num1 : " + num2 + ", num2 : " + num3 + "\nnum2가 num1보다 크다"
+				: "num1 : " + num2 + ", num2 : " + num3 + "\nnum1가 num2보다 크다";
 		System.out.println(b);
 
 	}
